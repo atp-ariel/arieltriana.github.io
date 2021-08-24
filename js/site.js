5ffcedb5b33a6949248eddb314e4a214bc644af0
ArielTriana <usich37@gmail.com>
2021-08-24 15:31:41 -0400

    biography

--- js/site.js
+++ js/site.js
@@ -1,3 +1,11 @@
-function show_animate_nav(nav_item){
-    nav_item.removeClass()
+function calculate_age() { 
+    var diff_ms = Date.now() - new Date("1999-11-01").getTime();
+    var age_dt = new Date(diff_ms); 
+  
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
+  
+  
+$(document).ready(function(){
+    $("#age").html(calculate_age());
+});
